afbc7d615f7fd5c44b14f78bc4bf34d08a6344b3
Rompe-Aire <angeloleonhart01@gmail.com>
2024-06-28 11:48:41 -0300

    Maquetacion V.0.2.3

--- src/app/mayorista/page.tsx
+++ src/app/mayorista/page.tsx
@@ -22,14 +22,12 @@ export default function Mayorista() {
                     <p>{descripcion}</p>
                 </div>
 
-                <form action="" className=" flex flex-col gap-4 items-center" id="contacto">
-                    <Input isRequired type="nombre" placeholder="Nombre" className="w-2/4 bg-yellow-500 max-md:w-10/12" />
-                    <Input isRequired type="email" placeholder="Email" className="w-2/4 bg-yellow-500 max-md:w-10/12" />
-                    <Input isRequired type="telefono" placeholder="Telefono" className="w-2/4 bg-yellow-500 max-md:w-10/12" />
-
-                    <Input isRequired type="empresa" placeholder="Nombre empresa" className="w-2/4 bg-yellow-500 max-md:w-10/12" />
-
-                    <Textarea isRequired placeholder="Mensaje" className="w-2/4 bg-yellow-500 max-md:w-10/12" />
+                <form action="" className="flex flex-col gap-4 items-center" id="contacto">
+                    <Input isRequired type="nombre" placeholder="Nombre" className="w-2/4 bg-white text-black max-md:w-10/12" />
+                    <Input isRequired type="email" placeholder="Email" className="w-2/4 bg-white text-black max-md:w-10/12" />
+                    <Input isRequired type="telefono" placeholder="Telefono" className="w-2/4 bg-white text-black max-md:w-10/12" />
+                    <Input isRequired type="empresa" placeholder="Nombre empresa" className="w-2/4 bg-white text-black max-md:w-10/12" />
+                    <Textarea isRequired placeholder="Mensaje" className="w-2/4 bg-white text-black max-md:w-10/12" />
                     <Button radius="full" className="bg-yellow-500">Enviar</Button>
                 </form>
             </section>
